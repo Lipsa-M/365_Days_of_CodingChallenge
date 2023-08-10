ee8d14ce03af0be0978c06d4f24c7cbf185167e4
Lipsa Mahanta <93856297+Lipsa-M@users.noreply.github.com>
2023-08-10 23:32:23 +0530

    Add files via upload

---
+++ Day-1.js
@@ -0,0 +1,20 @@
+// 1. Write a function that takes two numbers as parameters and returns their Sum
+function sum(a,b)
+{
+    const num1=parseFloat(prompt("Enter first number:"));
+    const num2=parseFloat(prompt("Enter second number:"));
+
+        const add=num1+num2;
+        return add;
+}
+    const result=sum();
+    console.log('The sum is:' + result);
+
+
+
+
+
+
+
+
+
