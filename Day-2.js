b5839ff6922b08f1140fad07e0c722430267e570
Lipsa Mahanta <93856297+Lipsa-M@users.noreply.github.com>
2023-08-11 23:56:26 +0530

    Add files via upload

---
+++ Day-2.js
@@ -0,0 +1,5 @@
+// 2. Create a program that calculates the area of a rectangle given its width and height
+const width= parseFloat(prompt("Enter width:"));;
+const height= parseFloat(prompt("Enter height:"));;
+const area= 2*(height+width);
+console.log(area);
