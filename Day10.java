5b416a10a6a2fd422b0be19a64b7550473d3e4a4
Lipsa Mahanta <93856297+Lipsa-M@users.noreply.github.com>
2023-08-19 22:33:25 +0530

    Create Day10.java

---
+++ Day10.java
@@ -0,0 +1,15 @@
+class Day10{
+    public String newcreat(){
+        // String msg="Hellow World";
+        return "hellow World";
+    }
+    public String createHelloWorld(){
+        return newcreat();
+        
+    }
+    public static void main(String[] args) {
+        createhellow_1 obj= new createhellow_1();
+
+        System.out.println(obj.createHelloWorld());
+    }
+}
